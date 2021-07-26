1c0ec562fb519022be6eddd18af0f79a093a71f7
kl3jvi <klejvisiper@gmail.com>
2021-07-26 13:35:03 +0200

    Implementing File Manager Entry

--- app/src/main/java/com/example/recipeapp/view/activities/AddUpdateRecipeActivity.kt
+++ app/src/main/java/com/example/recipeapp/view/activities/AddUpdateRecipeActivity.kt
@@ -1,12 +1,48 @@
 package com.example.recipeapp.view.activities
 
+import android.app.Dialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 import com.example.recipeapp.R
+import com.example.recipeapp.databinding.ActivityAddUpdateRecipeBinding
+import com.example.recipeapp.databinding.CustomDialogBinding
 
-class AddUpdateRecipeActivity : AppCompatActivity() {
+class AddUpdateRecipeActivity : AppCompatActivity(), View.OnClickListener {
+
+    private lateinit var mBinding: ActivityAddUpdateRecipeBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_add_update_recipe)
+        mBinding = ActivityAddUpdateRecipeBinding.inflate(layoutInflater)
+        setContentView(mBinding.root)
+
+        mBinding.ivAddDishImage.setOnClickListener(this)
+    }
+
+    override fun onClick(v: View?) {
+        if (v != null) {
+            when (v.id) {
+                R.id.iv_add_dish_image -> {
+                    customImageSelectionDialog()
+                    return
+                }
+            }
+        }
+    }
+
+    private fun customImageSelectionDialog() {
+        val dialog = Dialog(this)
+        val binding: CustomDialogBinding =
+            CustomDialogBinding.inflate(layoutInflater)
+        dialog.setContentView(binding.root)
+        binding.tvCamera.setOnClickListener{
+            Toast.makeText(this, "Camera Clicked", Toast.LENGTH_SHORT).show()
+        }
+        binding.tvGallery.setOnClickListener{
+            Toast.makeText(this, "Gallery Clicked", Toast.LENGTH_SHORT).show()
+        }
+        dialog.show()
     }
 }
--- app/src/main/java/com/example/recipeapp/view/fragments/AllRecipesFragment.kt
+++ app/src/main/java/com/example/recipeapp/view/fragments/AllRecipesFragment.kt
@@ -1,34 +1,40 @@
 package com.example.recipeapp.view.fragments
 
+import android.content.Intent
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
-import com.example.recipeapp.databinding.FragmentHomeBinding
+import com.example.recipeapp.R
+import com.example.recipeapp.databinding.FragmentAlldishesBinding
+import com.example.recipeapp.view.activities.AddUpdateRecipeActivity
 import com.example.recipeapp.viewmodel.HomeViewModel
 
 class AllRecipesFragment : Fragment() {
 
     private lateinit var homeViewModel: HomeViewModel
-    private var _binding: FragmentHomeBinding? = null
+    private var _binding: FragmentAlldishesBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
     private val binding get() = _binding!!
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setHasOptionsMenu(true)
+    }
+
     override fun onCreateView(
-            inflater: LayoutInflater,
-            container: ViewGroup?,
-            savedInstanceState: Bundle?
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
     ): View? {
         homeViewModel =
-                ViewModelProvider(this).get(HomeViewModel::class.java)
+            ViewModelProvider(this).get(HomeViewModel::class.java)
 
-        _binding = FragmentHomeBinding.inflate(inflater, container, false)
+        _binding = FragmentAlldishesBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
         val textView: TextView = binding.textHome
@@ -42,5 +48,20 @@ class AllRecipesFragment : Fragment() {
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.menu_all_recipes, menu)
+        super.onCreateOptionsMenu(menu, inflater)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.action_add_recipe -> {
+                startActivity(Intent(requireActivity(), AddUpdateRecipeActivity::class.java))
+                return true
+            }
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
