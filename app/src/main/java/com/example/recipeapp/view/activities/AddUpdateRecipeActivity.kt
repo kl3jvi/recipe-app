4d13a391813d02e8fcee617abbe69f321c200226
kl3jvi <klejvisiper@gmail.com>
2021-07-26 16:02:19 +0200

    Implementing Permissions For Camera And Storage

--- app/src/main/java/com/example/recipeapp/view/activities/AddUpdateRecipeActivity.kt
+++ app/src/main/java/com/example/recipeapp/view/activities/AddUpdateRecipeActivity.kt
@@ -1,5 +1,6 @@
 package com.example.recipeapp.view.activities
 
+import android.Manifest
 import android.app.Dialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -8,8 +9,10 @@ import android.widget.Toast
 import com.example.recipeapp.R
 import com.example.recipeapp.databinding.ActivityAddUpdateRecipeBinding
 import com.example.recipeapp.databinding.CustomDialogBinding
+import pub.devrel.easypermissions.EasyPermissions
 
-class AddUpdateRecipeActivity : AppCompatActivity(), View.OnClickListener {
+class AddUpdateRecipeActivity : AppCompatActivity(), View.OnClickListener,
+    EasyPermissions.PermissionCallbacks {
 
     private lateinit var mBinding: ActivityAddUpdateRecipeBinding
 
@@ -37,12 +40,44 @@ class AddUpdateRecipeActivity : AppCompatActivity(), View.OnClickListener {
         val binding: CustomDialogBinding =
             CustomDialogBinding.inflate(layoutInflater)
         dialog.setContentView(binding.root)
-        binding.tvCamera.setOnClickListener{
-            Toast.makeText(this, "Camera Clicked", Toast.LENGTH_SHORT).show()
+        binding.tvCamera.setOnClickListener {
+            checkPermission()
+            dialog.dismiss()
         }
-        binding.tvGallery.setOnClickListener{
+        binding.tvGallery.setOnClickListener {
             Toast.makeText(this, "Gallery Clicked", Toast.LENGTH_SHORT).show()
+            dialog.dismiss()
         }
         dialog.show()
+    }
+
+    override fun onPermissionsGranted(requestCode: Int, perms: MutableList<String>) {}
+
+    override fun onPermissionsDenied(requestCode: Int, perms: MutableList<String>) {}
+
+
+    private fun checkPermission() {
+        if (hasPermission()) {
+            // Have permission, do the thing!
+            Toast.makeText(this, "TODO: Camera things", Toast.LENGTH_LONG).show()
+        } else {
+            // Ask for one permission
+            EasyPermissions.requestPermissions(
+                this,
+                getString(R.string.rationale_camera),
+                123,
+                Manifest.permission.CAMERA,
+                Manifest.permission.READ_EXTERNAL_STORAGE,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE
+            )
+        }
+    }
+
+    private fun hasPermission(): Boolean {
+        return EasyPermissions.hasPermissions(
+            this,
+            Manifest.permission.CAMERA
+        ) && EasyPermissions.hasPermissions(this, Manifest.permission.READ_EXTERNAL_STORAGE)
+                || EasyPermissions.hasPermissions(this, Manifest.permission.WRITE_EXTERNAL_STORAGE)
     }
 }
