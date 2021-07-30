36f2e196342360a2ea364ec5ac8745c0d89e02e3
kl3jvi <klejvisiper@gmail.com>
2021-07-30 23:47:20 +0200

    Implementing Room Database

---
+++ app/src/main/java/com/example/recipeapp/model/database/FavDishRoomDatabase.kt
@@ -0,0 +1,30 @@
+package com.example.recipeapp.model.database
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import com.example.recipeapp.model.entities.FavDish
+
+@Database(entities = [FavDish::class], version = 1)
+abstract class FavDishRoomDatabase : RoomDatabase() {
+    companion object {
+        @Volatile
+        private var INSTANCE: FavDishRoomDatabase? = null
+
+        fun getDatabase(context: Context): FavDishRoomDatabase {
+            // if the INSTANCE is not null, then return it,
+            // if it is, then create the database
+            return INSTANCE ?: synchronized(this) {
+                val instance = Room.databaseBuilder(
+                    context.applicationContext,
+                    FavDishRoomDatabase::class.java,
+                    "word_database"
+                ).build()
+                INSTANCE = instance
+                // return instance
+                instance
+            }
+        }
+    }
+}
