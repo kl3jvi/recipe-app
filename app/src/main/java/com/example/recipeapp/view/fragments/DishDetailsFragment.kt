72966136fafecdcd8e1c2e9db24e2da5b662f047
kl3jvi <klejvisiper@gmail.com>
2021-08-03 13:30:17 +0200

    Passing parameters to dish details with safe args

--- app/src/main/java/com/example/recipeapp/view/fragments/DishDetailsFragment.kt
+++ app/src/main/java/com/example/recipeapp/view/fragments/DishDetailsFragment.kt
@@ -1,16 +1,19 @@
 package com.example.recipeapp.view.fragments
 
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.navArgs
+import com.bumptech.glide.Glide
 import com.example.recipeapp.R
+import com.example.recipeapp.databinding.FragmentDishDetailsBinding
+import java.io.IOException
 
 class DishDetailsFragment : Fragment() {
 
+    private var mBinding: FragmentDishDetailsBinding? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -21,13 +24,32 @@ class DishDetailsFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_dish_details, container, false)
+        mBinding =
+            FragmentDishDetailsBinding.inflate(inflater, container, false)
+        return mBinding!!.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         val args: DishDetailsFragmentArgs by navArgs()
-        Log.e("Title", args.dishDetails.title)
-        Log.e("Type", args.dishDetails.title)
+
+        args.let {
+            try {
+                Glide.with(requireActivity())
+                    .load(it.dishDetails.image)
+                    .centerCrop()
+                    .into(mBinding!!.ivDishImage)
+            } catch (e: IOException) {
+                e.printStackTrace()
+            }
+            mBinding!!.dtTitle.text = it.dishDetails.title
+            mBinding!!.dtType.text = it.dishDetails.type
+            mBinding!!.dtCategory.text = it.dishDetails.category
+            mBinding!!.dtIngredientsVal.text = it.dishDetails.ingredients
+            mBinding!!.dtDirectionVal.text = it.dishDetails.cookingDirection
+            mBinding!!.dtTime.text =
+                resources.getString(R.string.cook_time_format, it.dishDetails.cookingTime)
+
+        }
     }
 }
