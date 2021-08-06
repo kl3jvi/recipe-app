d24eb3489c2f5406a9710119d972cb2acb9682d8
kl3jvi <klejvisiper@gmail.com>
2021-08-06 14:03:26 +0200

    RxJava Implementation

---
+++ app/src/main/java/com/example/recipeapp/viewmodel/RandomDishViewModel.kt
@@ -0,0 +1,43 @@
+package com.example.recipeapp.viewmodel
+
+import androidx.lifecycle.MutableLiveData
+import com.example.recipeapp.model.entities.RandomDish
+import com.example.recipeapp.model.network.RandomDishApiService
+import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
+import io.reactivex.rxjava3.disposables.CompositeDisposable
+import io.reactivex.rxjava3.observers.DisposableSingleObserver
+import io.reactivex.rxjava3.schedulers.Schedulers
+
+class RandomDishViewModel {
+    private val randomRecipeApiService = RandomDishApiService()
+    private val compositeDisposable = CompositeDisposable()
+
+    val loadRandomDish = MutableLiveData<Boolean>()
+    val randomDishResponse = MutableLiveData<RandomDish.Recipes>()
+    val randomDishLoadingError = MutableLiveData<Boolean>()
+
+    fun getRandomRecipeFromAPI() {
+        loadRandomDish.value = true
+        compositeDisposable.add(
+            randomRecipeApiService.getRandomDishes()
+                .subscribeOn(Schedulers.newThread())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribeWith(object : DisposableSingleObserver<RandomDish.Recipes>(){
+                    override fun onSuccess(value: RandomDish.Recipes?) {
+                        loadRandomDish.value = false
+                        randomDishResponse.value = value!!
+                        randomDishLoadingError.value = false
+                    }
+
+                    override fun onError(e: Throwable?) {
+                        loadRandomDish.value = false
+                        randomDishLoadingError.value = true
+                        e!!.printStackTrace()
+                    }
+
+                })
+
+
+        )
+    }
+}
